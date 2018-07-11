baf5343605fcdd09c953bbfbabc2ef11418ec9bb
李珂昕 <likexin@likexindeMacBook-Pro.local>
2018-07-11 23:24:43 +0800

    stampTopo

--- src/Handle_MEO_Access/HandleSLinks.py
+++ src/Handle_MEO_Access/HandleSLinks.py
@@ -99,52 +99,6 @@ def TopoStamps(Original_topo_stamps):
     return TopoStamps
 
 
-#def SatelliteTopoDesign(ClsSortedList):
-#   def HandleSLinks(SatelliteOrignalLinks):
-#     SingleItems = []
-#     for SingleSatelliteOrignalLinks in SatelliteOrignalLinks:
-#         for SingleSchedSLinks in SingleSatelliteOrignalLinks:
-#             SingleItems.append(SingleSchedSLinks)
-#     return SingleItems
-def HandleLine(rec):
-
-
-    # line = f.readline()
-    # SingleSatelliteLinks = []
-    # while line:
-    #
-    #     if line.startswith("Global Statistics"):
-    #         for i in range(7):
-    #             f.readline()
-    #     if line.startswith("MEO") or line.startswith("LEO") or line.startswith("GEO"):
-    #         nodes = line.split("-To-")
-    #         f.readline()
-    #         f.readline()
-    #         f.readline()
-    #
-    #         if line.startswith("Min Elevation"):
-    #             for i in range(6):
-    #                 f.readline()
-    #
-    #
-    #         records = []
-    #         line = f.readline()
-    #         while len(line) > 2:
-    #             records.append(line)
-    #             line = f.readline()
-    #         SingleSatelliteLinks.append(HandleSingleRecords(nodes, records))
-    #     else:
-    #         if not line:
-    #             break
-    #         line = f.readline()
-    # return SingleSatelliteLinks
-
-    pass
-
-
-
-
-
 
 if __name__ == "__main__":
     files = os.listdir(DATA_PATH)
